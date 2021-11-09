b0ec1274e204e389d8453f3eab6708b3aa5805b1
Thao Phung <ttp7428@truman.edu>
2021-11-09 16:00:26 -0600

    added logs for db connection

--- server.js
+++ server.js
@@ -16,12 +16,18 @@ const env = process.env.NODE_ENV;
 log.info('environment', process.env.NODE_ENV);
 
 switch (env) {
-  case 'develop':
+  case 'develop': {
+    log.info('connecting to develop db', process.env.MONGODB_DEVELOP);
     connectionUrl = process.env.MONGODB_DEVELOP;
-  case 'production':
+  }
+  case 'production': {
+    log.info('connecting to production db', process.env.MONGODB_PRODUCTION);
     connectionUrl = process.env.MONGODB_PRODUCTION;
-  case 'local':
+  }
+  case 'local': {
+    log.info('connecting to local db', process.env.MONGODB_LOCAL);
     connectionUrl = process.env.MONGODB_LOCAL;
+  }
   default:
     connectionUrl = process.env.MONGODB_LOCAL;
 }
